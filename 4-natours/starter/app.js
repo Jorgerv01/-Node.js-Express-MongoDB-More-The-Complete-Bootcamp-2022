1eb470ab2860e6ae4c27e324f2f0b22f7f27fbfb
Jorge Ruiz <jorgeruiz@atbim.es>
2023-01-31 09:36:22 +0100

    section 14

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,36 +1,41 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 // standard
 const app = express();
 
+// Middleware
+app.use(morgan('dev'));
 app.use(express.json());
-
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
-const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
-
-// Obtener todos los tours
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-// Obtener un tour en concreto
-app.get('/api/v1/tours/:id', (req, res) => {
+// Sacamos cada tour de nuestra base de datos ficticia
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+
+// Creamos la funciones CRUD
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length)
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour here...',
+    },
+  });
+};
+
+const getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
   if (!tour)
@@ -45,24 +50,32 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+};
 
-// Modificar un tour con patch (modificar propiedades del objeto)
-app.patch('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length)
     return res.status(404).json({
       status: 'failed',
       message: 'Invalid ID',
     });
-  res.status(200).json({
+  res.status(204).json({
     status: 'success',
-    data: {
-      tour: 'Updated tour here...',
-    },
+    data: null,
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -79,8 +92,28 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
+// // Obtener todos los tours
+// app.get('/api/v1/tours', getAllTours);
+// // Obtener un tour en concreto
+// app.get('/api/v1/tours/:id', getTour);
+// // Modificar un tour con patch (modificar propiedades del objeto)
+// app.patch('/api/v1/tours/:id', updateTour);
+// // Eliminar un tour
+// app.delete('/api/v1/tours/:id', deleteTour);
+// // Crear un nuevo tour
+// app.post('/api/v1/tours', createTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// Iniciar servidor a traves de un determinado puerto
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
